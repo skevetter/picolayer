2e4a10a47f342fef74fe996c9bf3cd2530aacc89
Samuel K <69881238+skevetter@users.noreply.github.com>
2025-11-01 21:53:39 -0400

    style: format shell scripts (#19)
    Signed-off-by: Samuel K <69881238+skevetter@users.noreply.github.com>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,12 +3,12 @@
 set -eo pipefail
 
 main() {
-  echo "Starting post-create setup in $(pwd)"
+    echo "Starting post-create setup in $(pwd)"
 
-  sudo cp ".devcontainer/welcome.txt" "/usr/local/etc/vscode-dev-containers/first-run-notice.txt"
-  pre-commit install --install-hooks || { echo "Pre-commit hooks failed to install"; }
+    sudo cp ".devcontainer/welcome.txt" "/usr/local/etc/vscode-dev-containers/first-run-notice.txt"
+    pre-commit install --install-hooks || { echo "Pre-commit hooks failed to install"; }
 
-  echo "Post-create setup complete!"
+    echo "Post-create setup complete!"
 }
 
 main "$@"
